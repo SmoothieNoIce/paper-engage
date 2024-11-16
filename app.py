4522257676769746b3e6a5bb8b26080783f087f9
SmoothieNoIce <dev@flexolk.me>
2024-11-17 00:12:49 +0800

    feat: 11/17

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained('bert-base-uncased')
 
 agent: DQN_agent = None
+attack_mapping_simple = None
 
 def get_ttp_by_command(command):
     
@@ -117,7 +118,8 @@ def preprocess_command(id, sid, command):
     return {
         'is_script': is_script,
         'commandvec': commandvec,
-        'ttp_label_int': ttp_label_int
+        'ttp_label_int': ttp_label_int,
+        'ttp': ttp
     }
 
 class ad_env:
@@ -138,26 +140,42 @@ class ad_env:
         self.id_list = [] # 用於存儲每個指令的 ID(時間戳)
         pass
 
-    def get_action(self, state):
+    def execute_action(self, state, sid):
+        my_data = {'sid': sid, "action": 1}
+        #r1 = requests.get(f"{app.config['engage_ad']}", data = my_data)
+        #r2 = requests.post(f"{app.config['engage_network']}/post", data = my_data)
         pass
     
-    def getReward(self, ttp_before, a_before):
-        current_reward = 
-
+    def get_reward(self, ttp_before, a_before):
+        current_reward = 0
+        
+        # Reward: MITRE Engage
         for action in predefined_actions:
+            # 先檢查 action 與 predefined_actions 的對應
             if action['id'] == a_before:
-                if ttp_before['techniques'][0]['technique_id'] in action['activities']:
-                    current_reward += 10
-                else:
-                    current_reward -= 10
+                if action['id'] in list(attack_mapping_simple.keys()):
+                    # 檢查每個 action 的 TTP
+                    engages_activity = attack_mapping_simple[action['id']]
+                    for ttp in ttp_before['techniques']:
+                        if ttp['id'] in list(attack_mapping_simple.keys()):
+                            ttp_activity = attack_mapping_simple[ttp['id']]
+                            intersection = engages_activity.intersection(ttp_activity)
+                            if len(intersection) > 0:
+                                current_reward += 10
+                            else:
+                                current_reward -= 5
 
+        # Reward: MITRE ATT&CK
         for ttp in ttp_before['techniques']:
             if ttp['technique_id'] in self.all_technique:
-                current_reward -= 5
+                current_reward -= 10
             else:
                 current_reward += 5
 
             self.all_technique.append(ttp['technique_id'])
+        
+        technique_set = set(self.all_technique)
+        writer.add_scalar('techniques', len(technique_set), global_step=adenv.total_steps)
             
         return current_reward
 
@@ -200,30 +218,32 @@ def next_step():
             if adenv.isFirstCommand:
                 '''對當前的指令選擇 action'''
                 a_next = agent.select_action(s_next, deterministic=False)
+                adenv.execute_action(a_next, sid)
                 adenv.isFirstCommand = False
             else:
                 '''分析上一個指令 reward'''
                 '''Todo: 比較上一個指令 technique 和上一個 action 的 activity 對應到的 technique 是否有重疊，'''
                 '''還有目前環境的狀態，計算 reward'''
-                r_before = adenv.getReward(adenv.ttp_before, adenv.a_before)
-                
+
+                r_before = adenv.get_reward(adenv.ttp_before, adenv.a_before)
+                writer.add_scalar('action', adenv.a_before, global_step=adenv.total_steps)
+                writer.add_scalar('reward', r_before, global_step=adenv.total_steps)
+
                 '''計算上一個指令的 reward，然後進行學習'''
-                agent.replay_buffer.add(ad_env.s_before.detach().numpy(), ad_env.a_before, r_before, s_next.detach().numpy(), dw)
+                agent.replay_buffer.add(adenv.s_before.detach().numpy(), adenv.a_before, r_before, s_next.detach().numpy(), dw)
                 '''對當前的指令選擇 action'''
                 a_next = agent.select_action(s_next, deterministic=False)
+                adenv.execute_action(a_next, sid)
 
             '''Update AD Env，Next Step'''
             adenv.technique_int_before = command_data['ttp_label_int']
             adenv.ttp_before = command_data['ttp']
 
             '''Rest API'''
-            my_data = {'sid': data['sid'], "action": 1}
-            #r1 = requests.get(f"{app.config['engage_ad']}", data = my_data)
-            #r2 = requests.post(f"{app.config['engage_network']}/post", data = my_data)
 
             '''儲存 State 作為計算 reward'''
-            ad_env.s_before = s_next
-            ad_env.a_before = a_next
+            adenv.s_before = s_next
+            adenv.a_before = a_next
 
             '''Update Q Network'''
             if adenv.total_steps != 0 and adenv.total_steps % opt.update_every == 0:
@@ -257,7 +277,7 @@ def next_step():
         else:
             dw = 1
             s_next = get_vector_by_command("exit")
-            r_before = adenv.getReward(adenv.ttp_before, adenv.a_before)
+            r_before = adenv.get_reward(adenv.ttp_before, adenv.a_before)
             if len(adenv.s_before) != 0:
                 agent.replay_buffer.add(adenv.s_before.detach().numpy(), adenv.a_before, r_before, s_next.detach().numpy(), dw)
             print(f"message: exit!")
@@ -298,7 +318,8 @@ if __name__ == '__main__':
 
     parser.add_argument('--save_interval', type=int, default=int(50), help='Model saving interval, in steps.')
     parser.add_argument('--update_every', type=int, default=5, help='training frequency')
-
+    parser.add_argument('--exp_noise', type=float, default=0.2, help='explore noise')
+    
     parser.add_argument('--gamma', type=float, default=0.99, help='Discounted Factor')
     parser.add_argument('--net_width', type=int, default=200, help='Hidden net width')
     parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate')
@@ -321,7 +342,6 @@ if __name__ == '__main__':
     print(f"state_dim: {opt.state_dim}")
     print(f"action_dim: {opt.action_dim}")
     print(f"algo_name: {algo_name}")
-    print(f"algo_name: {algo_name}")
 
     adenv: ad_env = ad_env()
     agent = DQN_agent(**vars(opt))
@@ -335,6 +355,8 @@ if __name__ == '__main__':
         writepath = 'runs/{}_S_'.format(algo_name) + timenow
         if os.path.exists(writepath): shutil.rmtree(writepath)
         writer = SummaryWriter(log_dir=writepath)
+        print(f"writepath: {writepath}")
+
 
     print("======Reading MITRE ATT&CK=====")
     ## 整理 MITRE ATT&CK 
@@ -351,7 +373,12 @@ if __name__ == '__main__':
     label_encoder = LabelEncoder()
     label_encoder.fit(techniques_id)
 
+    ## config
+    with open('json/attack_mapping_simple.json', 'r') as attack_mapping_simple_file:
+        attack_mapping_simple = json.load(attack_mapping_simple_file)
+
+
     cudnn.benchmark = True
 
     print("======Start Engage=====")
-    app.run(host=app.config['host'], port=app.config['port'], debug=app.config['debug'])
+    app.run(host=app.config['host'], port=app.config['port'])
